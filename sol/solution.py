9de12d9f11e25c4f908ef23e7e1cbd2e8807319f
Eko229 <ekoguandor229@gmail.com>
2019-10-23 16:10:23 +0200

    cleanup

--- sol/solution.py
+++ sol/solution.py
@@ -2,7 +2,6 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import statsmodels.formula.api as sm
 import numpy as np
-from statsmodels.tools.eval_measures import mse
 
 
 def main():
@@ -11,17 +10,6 @@ def main():
     training_data = pd.read_csv(training_data_input, usecols=[0, 1, 2, 3, 4, 5, 6, 7, 8])
     test_data_predictors = pd.read_csv(test_data_input, usecols=[0, 1, 2, 3, 4, 5, 6, 7])
     test_data_response = pd.read_csv(test_data_input, usecols=[8])
-
-    # Parameters
-    # x1 = training_data.PowTotAct_Min
-    # x2 = training_data.Inj1PosVolAct_Var
-    # x3 = training_data.Inj1PrsAct_meanOfInjPhase
-    # x4 = training_data.Inj1HopTmpAct_1stPCscore
-    # x5 = training_data.Inj1HtgEd3Act_1stPCscore
-    # x6 = training_data.ClpFceAct_1stPCscore
-    # x7 = training_data.ClpPosAct_1stPCscore
-    # x8 = training_data.OilTmp1Act_1stPCscore
-    # y = training_data.mass
 
     # 1. Correlation
     print("Correlation")
@@ -126,6 +114,14 @@ def main():
     print("")
 
     # Interaction terms
+    formula = "mass ~ I(Inj1PrsAct_meanOfInjPhase ** 2)"
+    model = sm.ols(formula, training_data).fit()
+    print("{} \n{}".format(model.rsquared, model.pvalues))
+    predictions = model.predict(test_data_predictors)
+    mean_squared_error = np.mean(np.square(test_data_response.mass - predictions))
+    print("Test-MSE: " + str(mean_squared_error))
+    print("")
+
     formula = "mass ~ I(Inj1PrsAct_meanOfInjPhase ** 2) + Inj1PosVolAct_Var"
     model = sm.ols(formula, training_data).fit()
     print("{} \n{}".format(model.rsquared, model.pvalues))
